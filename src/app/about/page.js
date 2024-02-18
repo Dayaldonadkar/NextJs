0e9b77617290e1965d089ebd2b399f7969f4a7b3
Dayal Sudhakar <Dayalpython@gmail.com>
2024-02-18 15:41:20 +0530

    created responsive about age

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -9,28 +9,28 @@ const AboutPage = () => {
   return (
     <div className="flex justify-center py-5">
       <div className=" w-[85%] lg:w-4/5 xl:w-4/6 lg:flex items-center">
-        <div className="space-y-6">
-          <h2 className="text-btn text-xl">About Agency</h2>
-          <h1 className="text-3xl">
+        <div className="space-y-6 xl:space-y-10 2xl:space-y-16 lg:w-11/12 xl:w-9/12 2xl:w-9/12">
+          <h2 className="text-btn text-xl font-semibold">About Agency</h2>
+          <h1 className="text-3xl lg:text-4xl 2xl:text-6xl xl:w-10/12 2xl:w-11/12 font-bold">
             We create digital ideas that are bigger, bolder, braver and better.
           </h1>
-          <p className="text-sm">
+          <p className="text-sm 2xl:text-lg lg:w-10/12">
             We create digital ideas that are bigger, bolder, braver and better.
             We believe in good ideas flexibility and precission We’re world’s
             Our Special Team best consulting & finance solution provider. Wide
             range of web and software development services.
           </p>
-          <div className="flex">
+          <div className="flex space-x-2 lg:space-x-10 font-bold">
             <div className="space-y-1">
-              <h1 className="text-btn">10 K+</h1>
+              <h1 className="text-btn lg:text-2xl">10 K+</h1>
               <p className="text-sm">Year of experience</p>
             </div>
             <div className="space-y-1">
-              <h1 className="text-btn">234 K+</h1>
+              <h1 className="text-btn lg:text-2xl">234 K+</h1>
               <p className="text-sm">People reached</p>
             </div>
             <div className="space-y-1">
-              <h1 className="text-btn">5 K+</h1>
+              <h1 className="text-btn lg:text-2xl">5 K+</h1>
               <p className="text-sm">Services and Plugin</p>
             </div>
           </div>
